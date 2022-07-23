5284b859a557fd826e8886878854c92f7735856a
Bijith <86356172+BijithBMaman@users.noreply.github.com>
2022-07-23 12:17:06 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,10 @@
-Hello
+'''
+Github -----> Github Desktop
+Linkdin Profile
+Inftq website
+Basics + Functions + OOPs + DSA + DBMS
+Interview :- Sat
+A :- Concept  - > 10 ques + 5-7 homework
+test -> 4
+B:- Content - Content
+'''
